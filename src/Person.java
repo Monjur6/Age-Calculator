6309d6bbfc18b4c27fadd7001a1b1739a8153423
Monjur Morshed <monjurmorshed386@gmail.com>
2023-01-01 22:09:25 +0600

    add

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+public class Person {
+    private int id;
+    private string firstname;
+    private string lastname;
+    private LocalDate dateOfBirth;
+    public int getId{ return id; }
+
+}
+
+
+
+
+/*
+import java.util.Scanner;
+
+
+public class Person
+{
+
+    system.out.println("Please Choose One")
+    string st = "Student";
+    string fc = "Faculty";
+    Scanner keyboard = new Scanner(System.in);
+    choice = keyboard.nextInt();
+
+    if (choice == 1)
+    {
+        choice = st;
+    }
+    else if (choice == 2)
+    {
+        choice = fc;
+    }
+    else if (choice > 2 || choice < 1)
+    {
+        System.out.println("Choose either one or two");
+        choice = -1;
+        agecal();
+    }
+}
+*/
